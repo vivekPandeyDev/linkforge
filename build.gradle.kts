67b9dd4146fb1130bd311e1516e8599385256e3e
vivekPandeyDev <codewithvivekpandey@gmail.com>
2026-02-07 12:59:46 +0530

    config : initial spring boot project linkforge

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.github.vivek"
+version = "1.0.0"
+description = "This project is built to demonstrate real-world backend engineering concepts used in large-scale systems such as Bitly, TinyURL, and internal redirect platforms at big tech companies."
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-redis-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/io/github/vivek/linkforge/LinkforgeApplication.java
@@ -0,0 +1,13 @@
+package io.github.vivek.linkforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkforgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkforgeApplication.class, args);
+    }
+
+}
